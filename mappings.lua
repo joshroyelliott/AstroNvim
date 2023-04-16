4ecec728955a1d471a95cce5ed7b39ce50881d8b
Josh Elliott <josh.roy.elliott@gmail.com>
2023-04-16 15:18:13 +1000

    Custom leap.nvim to not use overwrite f and t

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -20,6 +22,11 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>vm"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown preview" },
+    ["<leader>vo"] = {
+      "<cmd>w<cr> :call system('/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD --colorscheme AstroNvim '.expand('%:p') . ' 2> /dev/null &')<cr>",
+      desc = "Open OpenSCAD",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
